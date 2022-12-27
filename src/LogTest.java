49c153ab4a437609bbcd119073d2af0f7f1c2924
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-12-27 13:51:37 +0100

    trying to add log4j

--- src/LogTest.java
+++ src/LogTest.java
@@ -1,10 +1,11 @@
 import java.util.logging.Logger;
 
 public class LogTest {
-    private static Logger logger= Logger.getLogger(String.valueOf(LogTest.class));
+    static Logger logger= Logger.getLogger(String.valueOf(LogTest.class));
 
     public static void main(String[] args){
         logger.info("hello test");
         System.out.println("hiii");
+        System.out.println(logger.getName());
     }
 }
