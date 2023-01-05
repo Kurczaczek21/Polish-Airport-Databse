c97e3ccd13cd9ae5fe0356420bd4dc354ca01176
Mateusz Warzecha <warzecha.mati@gmail.com>
2023-01-05 12:39:02 +0100

    LOG4j and Jsoup working :)))

--- src/LogTest.java
+++ src/LogTest.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.Logger;
 public class LogTest {
     private static Logger logger= LogManager.getLogger(LogTest.class);
     //Jtable filtrowanie !
+    // array lista z danymi i ich uzywa inna aplikacja
 
     public static void main(String[] args){
         System.out.println("start \n");
