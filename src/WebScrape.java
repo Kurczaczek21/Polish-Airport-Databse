ffad681212431986934caba871b05e8296ef9abf
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2023-01-07 00:45:16 +0100

    before excel

--- src/WebScrape.java
+++ src/WebScrape.java
@@ -10,7 +10,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.lang.reflect.Array;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -22,7 +27,7 @@ public class WebScrape {
 
         FirefoxOptions options = new FirefoxOptions();
         options.addPreference("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.170").addArguments("--headless");
-        WebDriver driver = new FirefoxDriver( new FirefoxOptions().addPreference("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.170").addArguments("--headless"));
+        WebDriver driver = new FirefoxDriver( new FirefoxOptions().addPreference("general.useragent.override","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.170"));
         driver.get(url);
         Thread.sleep(4000);
 
@@ -57,13 +62,39 @@ public class WebScrape {
         Thread.sleep(4000);
         Thread.sleep(4000);
 
+        JFrame f=new JFrame();
+        String columns[]={"DATE","TIME","AM/PM", "FLIGHT", "FROM","SHORT", "AIRLINE", "MODEL","AIRCFAT ID", "STATUS"};
+        DefaultTableModel model = new DefaultTableModel();
+        JTable jt = new JTable(model);
+        for (String s:columns
+             ) {
+            model.addColumn(s);
+        }
+
+
+
         List<WebElement> allHeaders = driver.findElements(By.xpath("//table[contains(@class,'table table-condensed table-hover data-table m-n-t-15')]//tr"));
         System.out.println(allHeaders.size());
         for(WebElement ele:allHeaders)
         {
-            logger.info(ele.getText());
-//            System.out.println(ele.getText());
+            String date;
+            if(ele.getText()=="") {
+                continue;
+            }
+            if(ele.getText().contains(",")){
+                date=ele.getText();
+                continue;
+            }
+            String line = ele.getText();
+            String lines[] = line.split("\\r?\\n");
+            String flightData[] = lines[0].split(" ");  // 0-> H , 1-> AM/PM , 2-> flight number
+            String airportData[] = lines[1].split(" "); // 0 -> airport, 1-> shortname
+            String planeData[] = lines[2].split(" "); // 0-> airlines, 1-> model, 2-> tag
+//            model.addRow(flightData);
+//            model.addRow(airportData);
+//            model.addRow(planeData);
         }
+
 
 //        driver.quit();
 
