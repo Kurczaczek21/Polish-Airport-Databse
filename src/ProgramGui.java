26d3534d30fdc10e1ccbf09e5ed14202c18fb612
Mateusz Warzecha <warzecha.mati@gmail.com>
2023-01-09 19:53:02 +0100

    added basic jpanel hello

--- src/ProgramGui.java
+++ src/ProgramGui.java
@@ -2,23 +2,44 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ProgramGui {
+
+    final static String HELLOSCREEN= "Card with hello screen message";
+    final static String CHOOSEDATA= "Card with boxes to download data";
     public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame();
         frame.setTitle("Poland Airports Statistics");
         ImageIcon image = new ImageIcon("project_data/images/plane_2.png");
         frame.setIconImage(image.getImage());
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(612*2,383*2);
-        frame.setVisible(true);
+//        frame.setResizable(false);
+        frame.setSize(800,500);    // 492   805
         Color aColor = Color.decode("#0174DF");     //#08298A
         frame.getContentPane().setBackground(aColor);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth()/2;
+        double height = screenSize.getHeight()/2;
+        frame.setLocation((int) width-(frame.getWidth()/2),(int) height-(frame.getHeight()/2));
+
 
         JLabel label = new JLabel();
+        label.setVisible(true);
         label.setText("Witam w wykresawce lotow");
         ImageIcon image2 = new ImageIcon("project_data/images/Entry_panel.jpg");
         label.setIcon(image2);
         label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(SwingConstants.TOP);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setFont(new Font("Serif", Font.PLAIN, 60));
         frame.add(label);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
+//        JPanel cards = new JPanel(new CardLayout());
+//        cards.add(frame.getContentPane(), HELLOSCREEN);
+//        cards.add(frame2.getContentPane(), CHOOSEDATA);
+
+
     }
 }
